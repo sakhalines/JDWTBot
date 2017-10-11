f0fd51b8a8031ea302157e456b96bc81b3f87b92
AnonPC\sakhalines <ahotna@gmail.com>
2017-10-12 01:50:19 +1000

    Перевод справки и сообщений

--- src/main/java/commands/essentials/Stats.java
+++ src/main/java/commands/essentials/Stats.java
@@ -1,6 +1,7 @@
 package commands.essentials;
 
 import commands.Command;
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.OnlineStatus;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
@@ -8,59 +9,57 @@ import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import utils.STATICS;
 
+import java.awt.*;
 import java.text.ParseException;
-import java.util.AbstractMap;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * © zekro 2017
  *
  * @author zekro
  */
+
 public class Stats implements Command {
 
-    private long getOnlineMembers(Guild guild) {
-        return guild.getMembers().stream()
-                .filter(member -> !member.getOnlineStatus().equals(OnlineStatus.OFFLINE))
-                .filter(member -> !member.getUser().isBot())
-                .count();
-    }
+    private class GuildStats {
 
-    private long getMembers(Guild guild) {
-        return guild.getMembers().stream().filter(member -> !member.getUser().isBot()).count();
-    }
+        private String name, id, region, avatar, afk, roles;
+        private int textChans, voiceChans, categories, rolesCount;
+        private long all, users, onlineUsers, bots, onlineBots;
+        private Member owner;
 
-    private HashMap<Role, Map.Entry<Integer, Integer>> getRoleMembersCount(Guild guild) {
+        private GuildStats(Guild g) {
 
-        HashMap<Role, Map.Entry<Integer, Integer>> out = new HashMap<>();
+            List<Member> l = g.getMembers();
 
-        for (Role role : guild.getRoles()) {
-            int count = 0;
-            int countOnline = 0;
-            for (Member member : guild.getMembers()) {
-                if (member.getRoles().contains(role)) {
-                    count++;
-                    if (!member.getOnlineStatus().equals(OnlineStatus.OFFLINE)) {
-                        countOnline++;
-                    }
-                }
-            }
-            out.put(role, new AbstractMap.SimpleEntry<>(count, countOnline));
+            this.name = g.getName();
+            this.id = g.getId();
+            this.region = g.getRegion().getName();
+            this.avatar = g.getIconUrl() == null ? "not set" : g.getIconUrl();
+            this.textChans = g.getTextChannels().size();
+            this.voiceChans = g.getVoiceChannels().size();
+            this.categories = g.getCategories().size();
+            this.rolesCount = g.getRoles().size();
+            this.afk = g.getAfkChannel() == null ? "not set" : g.getAfkChannel().getName();
+            this.owner = g.getOwner();
+
+            this.all = l.size();
+            this.users = l.stream().filter(m -> !m.getUser().isBot()).count();
+            this.onlineUsers = l.stream().filter(m -> !m.getUser().isBot() && !m.getOnlineStatus().equals(OnlineStatus.OFFLINE)).count();
+            this.bots = l.stream().filter(m -> m.getUser().isBot()).count();
+            this.onlineBots = l.stream().filter(m -> m.getUser().isBot() && !m.getOnlineStatus().equals(OnlineStatus.OFFLINE)).count();
+
+            this.roles = g.getRoles().stream()
+                    .filter(r -> !r.getName().contains("everyone"))
+                    .map(r -> String.format("%s (`%d`)", r.getName(), getMembsInRole(r)))
+                    .collect(Collectors.joining(", "));
         }
 
-        return out;
+        long getMembsInRole(Role r) {
+            return r.getGuild().getMembers().stream().filter(m -> m.getRoles().contains(r)).count();
+        }
     }
-
-    private String getRoleInfos(HashMap<Role, Map.Entry<Integer, Integer>> map) {
-        StringBuilder sb = new StringBuilder();
-        map.forEach((role, integerIntegerEntry) -> {
-            if (map.get(role).getKey() != 0)
-                sb.append("      - **" + role.getName() + ":**    " + map.get(role).getKey() + "  (Online: " + map.get(role).getValue() + ")" + "\n");
-        });
-        return sb.toString();
-    }
-
 
     @Override
     public boolean called(String[] args, MessageReceivedEvent event) {
@@ -70,16 +69,34 @@ public class Stats implements Command {
     @Override
     public void action(String[] args, MessageReceivedEvent event) throws ParseException {
 
-        Guild guild = event.getGuild();
-        event.getTextChannel().sendMessage(
-                "*" + guild.getName() + "*  -  **Статискиа сервера**\n\n" +
-                        " - **Имя сервера:**    " + guild.getName() + "\n" +
-                        " - **ID сервера:**    " + guild.getId() + "\n" +
-                        " - **Владелец сервера:**    " + guild.getOwner().getAsMention() + " (" + guild.getOwner().getOnlineStatus().toString() + ") " + "\n" +
-                        " - **Участники:**    " + getMembers(guild) + " (Онлайн: " + getOnlineMembers(guild) + ")" + "\n" +
-                        " - **Роли:** " + "\n" + getRoleInfos(getRoleMembersCount(guild))
-        ).queue();
+        Guild g = event.getGuild();
 
+        GuildStats gs = new GuildStats(g);
+
+        String usersText = String.format(
+                "**Всего пользователей:**   %d\n" +
+                "**Участников:**   %d   (Online:  %d)\n" +
+                "**Ботов:**   %d   (Online:  %d)",
+                gs.all, gs.users, gs.onlineUsers, gs.bots, gs.onlineBots
+        );
+
+        EmbedBuilder eb = new EmbedBuilder()
+                .setColor(Color.cyan)
+                .setTitle(gs.name + "  -  СТАТИСТИКА СЕРВЕРА")
+                .addField("Имя:", gs.name, false)
+                .addField("ID:", gs.id, false)
+                .addField("Владелец:", gs.owner.getUser().getName() + "#" + gs.owner.getUser().getDiscriminator(), false)
+                .addField("Регион:", gs.region, false)
+                .addField("Каналы", "**Текстовые каналы:**  " + gs.textChans + "\n**Голосовые каналы:**  " + gs.voiceChans, false)
+                .addField("Участники:", usersText, false)
+                .addField("Роли (" + gs.rolesCount + "): ", gs.roles, false)
+                .addField("AFK Канал", gs.afk, false)
+                .addField("Аватар", gs.avatar, false);
+
+        if (!gs.avatar.equals("not set"))
+            eb.setThumbnail(gs.avatar);
+
+        event.getTextChannel().sendMessage(eb.build()).queue();
     }
 
     @Override
@@ -89,12 +106,12 @@ public class Stats implements Command {
 
     @Override
     public String help() {
-        return "Использование: " + STATICS.PREFIX + "stats";
+        return "Использование: .stats";
     }
 
     @Override
     public String description() {
-        return "Получение статистики сервера";
+        return "Просмотр информации о сервере.";
     }
 
     @Override
@@ -104,6 +121,6 @@ public class Stats implements Command {
 
     @Override
     public int permission() {
-        return 1;
+        return 0;
     }
 }
