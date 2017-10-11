bb8a512d342620d30cef76790aedeb3ee6f34649
AnonPC\sakhalines <ahotna@gmail.com>
2017-10-11 16:23:05 +1000

    Добавлены параметры GAME_CHANGELOG_CHANNEL_PREFIX и GAME_CHANGELOG_UPDATE_INTERVAL, чтобы можно было их менять с помощью команды setting

--- src/main/java/core/SSSS.java
+++ src/main/java/core/SSSS.java
@@ -6,9 +6,7 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import utils.STATICS;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -29,38 +27,72 @@ public class SSSS /* Stands for "SERVER SPECIFIC SETTINGS SYSTEM" :^) */ {
         });
     }
 
+//    public static void listSettings(MessageReceivedEvent event) {
+//
+//        Guild g = event.getGuild();
+//
+//        StringBuilder keys = new StringBuilder();
+//        StringBuilder values = new StringBuilder();
+//
+//        File[] files = new File("SERVER_SETTINGS/" + g.getId() + "/").listFiles();
+//        System.out.println(files.length);
+//
+//        Arrays.stream(files).forEach(f -> {
+//            try {
+//                BufferedReader br = new BufferedReader(new FileReader(f));
+//                keys.append("**" + f.getName() + "**\n");
+//                values.append("`" + br.readLine() + "`\n");
+//                br.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//
+//        event.getTextChannel().sendMessage(new EmbedBuilder()
+//                .setTitle("SettingsCore for guild \"" + g.getName() + "\" (" + g.getId() + ")", null)
+//                .addBlankField(false)
+//                .addField("Keys", keys.toString(), true)
+//                .addField("Values", values.toString(), true)
+//                .build()
+//        ).queue();
+//
+//    }
+
     public static void listSettings(MessageReceivedEvent event) {
 
         Guild g = event.getGuild();
 
-        StringBuilder keys = new StringBuilder();
-        StringBuilder values = new StringBuilder();
+        HashMap<String, String> sets = new HashMap<>();
+        sets.put("PREFIX                ", getPREFIX(g));
+        sets.put("SERVER_JOIN_MSG       ", getSERVERJOINMESSAGE(g));
+        sets.put("SERVER_LEAVE_MSG      ", getSERVERJOINMESSAGE(g));
+        sets.put("VKICK_CHANNEL         ", getVKICKCHANNEL(g));
+        
+        sets.put("GAME_CHANGELOG_CHANNEL_PREFIX     ", getGAMECHANGELOGCHANNELPREFIX(g));
+        sets.put("GAME_CHANGELOG_UPDATE_INTERVAL    ", getGAMECHANGELOGUPDATEINTERVAL(g));
 
-        File[] files = new File("SERVER_SETTINGS/" + g.getId() + "/").listFiles();
-        System.out.println(files.length);
+        HashMap<String, String> setsMulti = new HashMap<>();
+        setsMulti.put("PERMROLES_LVL1", String.join(", ", getPERMROLES_1(g)));
+        setsMulti.put("PERMROLES_LVL2", String.join(", ", getPERMROLES_2(g)));
+        setsMulti.put("PERMROLES_LVL2", String.join(", ", getPERMROLES_3(g)));
+        setsMulti.put("BLACKLIST", String.join(", ", getBLACKLIST(g)));
 
-        Arrays.stream(files).forEach(f -> {
-            try {
-                BufferedReader br = new BufferedReader(new FileReader(f));
-                keys.append("**" + f.getName() + "**\n");
-                values.append("`" + br.readLine() + "`\n");
-                br.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+        StringBuilder sb = new StringBuilder()
+                .append("**Список настроек этого сервера**\n\n")
+                .append("```")
+                .append("ПАРАМЕТР            -  ЗНАЧЕНИЕ\n\n");
 
-        event.getTextChannel().sendMessage(new EmbedBuilder()
-                .setTitle("SettingsCore for guild \"" + g.getName() + "\" (" + g.getId() + ")", null)
-                .addBlankField(false)
-                .addField("Keys", keys.toString(), true)
-                .addField("Values", values.toString(), true)
-                .build()
-        ).queue();
+        sets.forEach((k, v) -> sb.append(
+                String.format("%s  -  \"%s\"\n", k, v))
+        );
 
+        sb.append("\n- - - - -\n\n");
+        setsMulti.forEach((k, v) -> sb.append(
+                String.format("%s:\n\"%s\"\n\n", k, v))
+        );
+
+        event.getTextChannel().sendMessage(new EmbedBuilder().setDescription(sb.append("```").toString()).build()).queue();
     }
-
-
 
 
     public static String getPREFIX(Guild guild) {
@@ -128,63 +160,6 @@ public class SSSS /* Stands for "SERVER SPECIFIC SETTINGS SYSTEM" :^) */ {
         settingWrite(entry, f);
     }
 
-
-//    public static String getMUSICCHANNEL(Guild guild) {
-//
-//        try {
-//            File f = new File("SERVER_SETTINGS/" + guild.getId() + "/musicchannel");
-//            if (f.exists()) {
-//                try {
-//                    return new BufferedReader(new FileReader(f)).readLine();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        } catch (Exception e) {}
-//        return "";
-//    }
-
-//    public static void setMUSICCHANNEL(String entry, Guild guild) {
-//
-//        File f = new File("SERVER_SETTINGS/" + guild.getId() + "/musicchannel");
-//        try {
-//            BufferedWriter r = new BufferedWriter(new FileWriter(f));
-//            r.write(entry);
-//            r.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-    //    public static boolean getLOCKMUSICCHANNEL(Guild guild) {
-//
-//        try {
-//            File f = new File("SERVER_SETTINGS/" + guild.getId() + "/lockmusicchannel");
-//            if (f.exists()) {
-//                try {
-//                    return new BufferedReader(new FileReader(f)).readLine().equals("true");
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        } catch (Exception e) {}
-//        return false;
-//    }
-
-//    public static void setLOCKMUSICCHANNEL(boolean entry, Guild guild) {
-//
-//        File f = new File("SERVER_SETTINGS/" + guild.getId() + "/lockmusicchannel");
-//        try {
-//            BufferedWriter r = new BufferedWriter(new FileWriter(f));
-//            r.write(entry + "");
-//            r.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-
-
     public static String[] getPERMROLES_1(Guild guild) {
 
         try {
@@ -251,16 +226,6 @@ public class SSSS /* Stands for "SERVER SPECIFIC SETTINGS SYSTEM" :^) */ {
         settingWrite(entry, f);
     }
     
-    private static void settingWrite(String entry, File f){
-        try {
-            BufferedWriter r = new BufferedWriter(new FileWriter(f));
-            r.write(entry);
-            r.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static String getVKICKCHANNEL(Guild guild) {
 
         try {
@@ -280,22 +245,6 @@ public class SSSS /* Stands for "SERVER SPECIFIC SETTINGS SYSTEM" :^) */ {
 
         File f = new File("SERVER_SETTINGS/" + guild.getId() + "/vkickchannel");
         settingWrite(entry, f);
-    }
-
-
-    public static String getR6OPSID(Guild guild) {
-
-        try {
-            File f = new File("SERVER_SETTINGS/" + guild.getId() + "/r6opsID");
-            if (f.exists()) {
-                try {
-                    return new BufferedReader(new FileReader(f)).readLine();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        } catch (Exception e) {}
-        return "OFF";
     }
 
     public static List<String> getBLACKLIST(Guild guild) {
@@ -329,4 +278,58 @@ public class SSSS /* Stands for "SERVER SPECIFIC SETTINGS SYSTEM" :^) */ {
         }
     }
 
+
+    public static String getGAMECHANGELOGCHANNELPREFIX(Guild guild) {
+
+        try {
+            File f = new File("SERVER_SETTINGS/" + guild.getId() + "/gamechangelogchannel");
+            if (f.exists()) {
+                try {
+                    return new BufferedReader(new FileReader(f)).readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {}
+        return "";
+    }
+
+    public static void setGAMECHANGELOGCHANNELPREFIX(String entry, Guild guild) {
+
+        File f = new File("SERVER_SETTINGS/" + guild.getId() + "/gamechangelogchannel");
+        settingWrite(entry, f);
+    }
+
+    public static String getGAMECHANGELOGUPDATEINTERVAL(Guild guild) {
+
+        try {
+            File f = new File("SERVER_SETTINGS/" + guild.getId() + "/gamechangelogupdateinterval");
+            if (f.exists()) {
+                try {
+                    return new BufferedReader(new FileReader(f)).readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {}
+        return "";
+    }
+
+    public static void setGAMECHANGELOGUPDATEINTERVAL(String entry, Guild guild) {
+
+        File f = new File("SERVER_SETTINGS/" + guild.getId() + "/gamechangelogupdateinterval");
+        settingWrite(entry, f);
+    }
+
+    private static void settingWrite(String entry, File f){
+        try {
+            if (!f.exists())
+                f.createNewFile();
+            BufferedWriter r = new BufferedWriter(new FileWriter(f));
+            r.write(entry);
+            r.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
