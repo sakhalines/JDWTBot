83feeacd29a4ac724cefb0804f5a1254c4ca3cda
AnonPC\sakhalines <ahotna@gmail.com>
2018-03-10 13:45:35 +1000

    Обновлён номер версии до 1.0.1

--- src/main/java/utils/STATICS.java
+++ src/main/java/utils/STATICS.java
@@ -13,7 +13,7 @@ public class STATICS {
     //######### GENERAL BOT SETTINGS #########//
 
 
-    public static String VERSION = "1.0";
+    public static String VERSION = "1.0.1";
     public static String THISBUILD = BUILDTYPE.STABLE;
 
     public static class BUILDTYPE {
