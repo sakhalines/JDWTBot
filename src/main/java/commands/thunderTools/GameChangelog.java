f7efc45e01710167f1e62bdd936e5a8e82a8860a
AnonPC\sakhalines <ahotna@gmail.com>
2018-04-27 00:03:56 +1000

    null

---
+++ src/main/java/commands/thunderTools/GameChangelog.java
@@ -0,0 +1,58 @@
+package commands.thunderTools;
+
+import commands.Command;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import utils.STATICS;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+public class GameChangelog implements Command {
+    @Override
+    public boolean called(String[] args, MessageReceivedEvent event) {
+        return false;
+    }
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent event) throws ParseException, IOException {
+        GetAndSendContent.sendContent(args, event, "gameChangelog");
+    }
+
+    @Override
+    public void executed(boolean success, MessageReceivedEvent event) {
+
+    }
+
+    @Override
+    public String help() {
+        String helpText = "**Получение списка обновлений игры War Thunder c официального сайта" +
+                "\nИспользование:**" +
+                "\n.gameChangelog [c] [f] [numder]" +
+                "\n**c** - послать ответ в текущий канал" +
+                "\n**C** - послать ответ в канал мониторинга обновлений" +
+                "\n**f** - показать подробности обновления" +
+                "\n**numder** - количество новостей для получения или порядковый номер новости при запросе подробностей (число от одного до десяти)." +
+                "\n\n**Примеры:**\n" +
+                "\n**'.gameChangelog 5'** пришлёт пять крайних обновлений. \n" +
+                "**'.gameChangelog c'** пришлёт только одно самое крайнее обновление в текущий канал.\n" +
+                "\n**'.gameChangelog c f 3 или .chlog cf 3'** пришлёт подробности третьего обновления." +
+                "\n\n**Алиасы** (сокращённая форма команды):\n" +
+                "\n**'.chlog'**";
+        return helpText;
+    }
+
+    @Override
+    public String description() {
+        return "Получение списка обновлений игры War Thunder c официального сайта";
+    }
+
+    @Override
+    public String commandType() {
+        return STATICS.CMDTYPE.thunderTools;
+    }
+
+    @Override
+    public int permission() {
+        return 1;
+    }
+}
---
+++ src/main/java/commands/thunderTools/GameNews.java
@@ -0,0 +1,58 @@
+package commands.thunderTools;
+
+import commands.Command;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import utils.STATICS;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+public class GameNews implements Command {
+    @Override
+    public boolean called(String[] args, MessageReceivedEvent event) {
+        return false;
+    }
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent event) throws ParseException, IOException {
+        GetAndSendContent.sendContent(args, event, "gameNews");
+    }
+
+    @Override
+    public void executed(boolean success, MessageReceivedEvent event) {
+
+    }
+
+    @Override
+    public String help() {
+        String helpText = "**Получение новостей игры War Thunder c официального сайта" +
+                "\nИспользование:**" +
+                "\n.gameNews [c] [f] [numder]" +
+                "\n**c** - послать ответ в текущий канал" +
+                "\n**C** - послать ответ в канал мониторинга обновлений" +
+                "\n**f** - показать подробности обновления" +
+                "\n**numder** - количество новостей для получения или порядковый номер новости при запросе подробностей (число от одного до десяти)." +
+                "\n\n**Примеры:**\n" +
+                "\n**'.gameNews 5'** пришлёт пять крайних новостей. \n" +
+                "**'.gameNews c'** пришлёт только одну самую крайнюю новость в текущий канал.\n" +
+                "\n**'.gameNews c f 3 или .gameNews cf 3'** пришлёт подробности третьей новости." +
+                "\n\n**Алиасы** (сокращённая форма команды):\n" +
+                "\n**'.gnews'**";
+        return helpText;
+    }
+
+    @Override
+    public String description() {
+        return "Получение новостей игры War Thunder c официального сайта";
+    }
+
+    @Override
+    public String commandType() {
+        return STATICS.CMDTYPE.thunderTools;
+    }
+
+    @Override
+    public int permission() {
+        return 1;
+    }
+}
