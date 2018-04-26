6fc14d2873da3ca2028bff9f7122ba5338ea863a
AnonPC\sakhalines <ahotna@gmail.com>
2018-04-27 06:56:14 +1000

    * Отображение префикса в справочном сообщении в соответствии с настройками сервера. * Форматирование справочного сообщения.

--- src/main/java/commands/thunderTools/GameChangelog.java
+++ src/main/java/commands/thunderTools/GameChangelog.java
@@ -27,17 +27,17 @@ public class GameChangelog implements Command {
     public String help() {
         String prefix = STATICS.PREFIX;
         String helpText = "**Получение списка обновлений игры War Thunder c официального сайта" +
-                "\nИспользование:\n\"" +
-                prefix + "gameChangelog [c] [f] [numder]\"**\n" +
+                "\n\nИспользование:\n" +
+                prefix + "gameChangelog [c] [f] [numder]**\n" +
                 "**c** - послать ответ в текущий канал\n" +
                 "**C** - послать ответ в канал мониторинга обновлений\n" +
                 "**f** - показать подробности обновления\n" +
                 "**numder** - количество новостей для получения или порядковый номер новости при запросе подробностей (число от одного до десяти)." +
-                "\n\n**Примеры:\n\"" +
-                prefix + "gameChangelog 5\"** - пришлёт пять крайних обновлений.\n**\"" +
-                prefix + "gameChangelog c\"** - пришлёт только одно самое крайнее обновление в текущий канал.\n\"**" +
-                prefix + "gameChangelog c f 3\"** или  **\"" + prefix + "chlog cf3\"** - пришлёт подробности третьего обновления." +
-                "\n\n**Алиасы** (сокращённая форма команды):    **\"" + prefix + "chlog\"**";
+                "\n\n**Примеры:\n" +
+                prefix + "gameChangelog 5** - пришлёт пять крайних обновлений.\n**" +
+                prefix + "gameChangelog c** - пришлёт только одно самое крайнее обновление в текущий канал.\n**" +
+                prefix + "gameChangelog c f 3** или  **" + prefix + "chlog cf3** - пришлёт подробности третьего обновления." +
+                "\n\n**Алиасы** (сокращённая форма команды):    **" + prefix + "chlog**";
         return helpText;
     }
 
